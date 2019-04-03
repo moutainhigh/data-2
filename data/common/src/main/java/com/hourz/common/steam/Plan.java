1d92f340d83f4d67287771d4e90e2912510db6b1
hourz <hourongzhen@casic27.com>
2019-04-03 15:06:43 +0800

    公司收尾

---
+++ data/common/src/main/java/com/hourz/common/steam/Plan.java
@@ -0,0 +1,53 @@
+package com.hourz.common.steam;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import com.sun.glass.ui.EventLoop.State;
+
+public class Plan {
+
+	private int id;    
+	private String planNo;    
+	private BigDecimal price;    
+	private long total;    
+	private State state;
+	private Calendar createTime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPlanNo() {
+		return planNo;
+	}
+	public void setPlanNo(String planNo) {
+		this.planNo = planNo;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public long getTotal() {
+		return total;
+	}
+	public void setTotal(long total) {
+		this.total = total;
+	}
+	public Calendar getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Calendar createTime) {
+		this.createTime = createTime;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+}
